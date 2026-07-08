94e9b3096b8d01500e44b406fb3fb51bca81c64d
ronardcaktus <rluna@caktusgroup.com>
2026-07-07 20:13:27 -0400

    Create curl-test command

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,9 @@
 import os
 import datetime
-from flask import Flask
+from flask import Flask, request
 from dotenv import load_dotenv
 from peewee import *
+from playhouse.shortcuts import model_to_dict
 
 from .routes import register_routes
 
@@ -27,8 +28,6 @@ class TimelinePost(Model):
 mydb.connect()
 mydb.create_tables([TimelinePost])
 
-
-
 def create_app():
     app = Flask(__name__)
 
@@ -38,3 +37,29 @@ def create_app():
 
 
 app = create_app()
+
+@app.route('/api/timeline_post', methods=['POST'])
+def post_time_line_post():
+    name = request.form[ 'name']
+    email = request.form['email']
+    content = request. form[ 'content' ]
+    timeline_post = TimelinePost. create(name=name, email=email, content=content)
+
+    return model_to_dict(timeline_post)
+
+@app.route( '/api/timeline_post', methods=['GET'])
+def get_time_line_post():
+    return {
+        'timeline_posts': [
+            model_to_dict(p)
+            for p in
+TimelinePost.select().order_by(TimelinePost.created_at.desc())
+        ]
+    }
+
+@app.route('/api/timeline_post/<int:post_id>', methods=['DELETE'])
+def delete_time_line_post(post_id):
+    deleted = TimelinePost.delete().where(TimelinePost.id == post_id).execute()
+    if deleted == 0:
+        return {'error': f'timeline post {post_id} not found'}, 404
+    return {'deleted': post_id}
